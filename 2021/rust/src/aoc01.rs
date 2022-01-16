8511feb9c7e2931cfa07dc3d9e4bed52c7368737
xehpuk <xehpuk.dev@gmail.com>
2022-01-16 02:17:01 +0100

    rust: solve 2021.01, part 1

---
+++ 2021/rust/src/aoc01.rs
@@ -0,0 +1,59 @@
+use std::io::Error;
+use std::num::ParseIntError;
+
+pub fn main() {
+    match read_input() {
+        Ok(text) => {
+            solve(part1, &text);
+        }
+        Err(err) => eprintln!("{:#?}", err),
+    }
+}
+
+fn solve(part: fn(&str) -> Result<i32, ParseIntError>, text: &str) {
+    match part(text) {
+        Ok(drift) => println!("{}", drift),
+        Err(err) => eprintln!("{:#?}", err),
+    }
+}
+
+fn part1(input: &str) -> Result<i32, ParseIntError> {
+    Ok(input
+        .lines()
+        .map(str::parse)
+        .collect::<Result<Vec<i32>, _>>()?
+        .windows(2)
+        .fold(0, |increased, depth| {
+            if depth[0] < depth[1] {
+                increased + 1
+            } else {
+                increased
+            }
+        }))
+}
+
+fn read_input() -> Result<String, Error> {
+    std::fs::read_to_string("../01.txt")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::part1;
+
+    const INPUT: &str = "\
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+    #[test]
+    fn test1() {
+        assert_eq!(Ok(7), part1(INPUT));
+    }
+}
--- 2021/rust/src/main.rs
+++ 2021/rust/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Bye, 2021!");
+use std::env::args;
+use std::num::ParseIntError;
+
+mod aoc01;
+
+fn main() -> Result<(), ParseIntError> {
+    match args().nth(1) {
+        Some(day) => match day.parse::<u8>()? {
+            1 => aoc01::main(),
+            n => eprintln!("Number must be in 1..=1, {} clearly isn't.", n),
+        },
+        None => {
+            println!(
+                "Running latest exercise (day 1). You may provide a number, e.g.: cargo run -- 2"
+            );
+            aoc01::main();
+        }
+    }
+
+    Ok(())
 }
