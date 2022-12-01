6c8ab54cfe95eca9c8bd3961e5f78fe86870c18a
xehpuk <xehpuk.dev@gmail.com>
2022-12-02 00:40:40 +0100

    rust: fix 2022.01, part 1; solve 2022.01, part 2

--- 2022/rust/src/aoc01.rs
+++ 2022/rust/src/aoc01.rs
@@ -1,4 +1,5 @@
-use std::cmp::max;
+use std::cmp::{max, Reverse};
+use std::collections::BinaryHeap;
 use std::io::Error;
 use std::num::ParseIntError;
 
@@ -22,6 +23,7 @@ fn solve(part: fn(&str) -> Result<i32, ParseIntError>, text: &str) {
 fn part1(input: &str) -> Result<i32, ParseIntError> {
     let mut most_calories = 0;
     let mut current_calories = 0;
+
     for line in input.lines() {
         if line.is_empty() {
             most_calories = max(most_calories, current_calories);
@@ -30,11 +32,43 @@ fn part1(input: &str) -> Result<i32, ParseIntError> {
             current_calories += line.parse::<i32>()?;
         }
     }
-    Ok(most_calories)
+
+    Ok(if current_calories > 0 {
+        max(most_calories, current_calories)
+    } else {
+        most_calories
+    })
 }
 
 fn part2(input: &str) -> Result<i32, ParseIntError> {
-    todo!()
+    let mut most_calories = BinaryHeap::new();
+    let mut current_calories = 0;
+
+    fn update_most_calories(most_calories: &mut BinaryHeap<Reverse<i32>>, current_calories: i32) {
+        if most_calories.len() < 3 {
+            most_calories.push(Reverse(current_calories));
+        } else {
+            let mut least_calories = most_calories.peek_mut().unwrap();
+            if current_calories > least_calories.0 {
+                *least_calories = Reverse(current_calories);
+            }
+        }
+    }
+
+    for line in input.lines() {
+        if line.is_empty() {
+            update_most_calories(&mut most_calories, current_calories);
+            current_calories = 0;
+        } else {
+            current_calories += line.parse::<i32>()?;
+        }
+    }
+
+    if current_calories > 0 {
+        update_most_calories(&mut most_calories, current_calories);
+    }
+
+    Ok(most_calories.iter().map(|calories| calories.0).sum())
 }
 
 fn read_input() -> Result<String, Error> {
@@ -45,6 +79,8 @@ fn read_input() -> Result<String, Error> {
 mod tests {
     use super::{part1, part2};
     use std::num::ParseIntError;
+
+    const INPUT: &str = "1000,2000,3000,,4000,,5000,6000,,7000,8000,9000,,10000";
 
     fn test_input(input: &str) -> String {
         input.split(",").collect::<Vec<_>>().join("\n")
@@ -60,17 +96,11 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(
-            Ok(24000),
-            test_part1("1000,2000,3000,,4000,,5000,6000,,7000,8000,9000,,10000")
-        );
+        assert_eq!(Ok(24000), test_part1(INPUT));
     }
 
     #[test]
     fn test2() {
-        assert_eq!(
-            Ok(45000),
-            test_part2("1000,2000,3000,,4000,,5000,6000,,7000,8000,9000,,10000")
-        );
+        assert_eq!(Ok(45000), test_part2(INPUT));
     }
 }
