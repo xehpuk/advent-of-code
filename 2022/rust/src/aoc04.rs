88ab0e5685e19a0e11fdc237451f37f0a14f029c
xehpuk <xehpuk.dev@gmail.com>
2022-12-05 01:12:24 +0100

    rust: solve 2022.04, part 1 horribly

--- 2022/rust/src/aoc04.rs
+++ 2022/rust/src/aoc04.rs
@@ -1,10 +1,63 @@
 use super::Day;
+use std::ops::RangeInclusive;
 
 pub struct Day04;
 
 impl<'a, I: Clone + Iterator<Item = &'a str>> Day<'a, I> for Day04 {
     fn part1(input: I) -> Option<i32> {
-        todo!()
+        let mut containing_assignment_pairs = 0;
+        for line in input {
+            let vec = line.split(',').collect::<Vec<_>>();
+            let vec = vec
+                .into_iter()
+                .map(|range| range.split('-').collect::<Vec<_>>())
+                .collect::<Vec<_>>();
+            let vec = vec
+                .into_iter()
+                .map(|range| {
+                    let start = range.get(0).unwrap().parse::<i32>().unwrap();
+                    let end = range.get(1).unwrap().parse::<i32>().unwrap();
+                    start..=end
+                })
+                .collect::<Vec<_>>();
+            let first_range = vec.get(0).unwrap();
+            let second_range = vec.get(1).unwrap();
+            if (first_range.start() <= second_range.start()
+                && first_range.end() >= second_range.end())
+                || (second_range.start() <= first_range.start()
+                    && second_range.end() >= first_range.end())
+            {
+                containing_assignment_pairs += 1;
+            }
+        }
+        Some(containing_assignment_pairs)
+        // let ranges = input.map(|line| line.split(',').collect::<Vec<_>>());
+        // let ids: Vec<_> = input
+        //     .flat_map(|line| line.split(','))
+        //     .flat_map(|range| range.split('-'))
+        //     .flat_map(|id| id.parse::<i32>().ok())
+        //     .collect();
+        //
+        // ids.chunks_exact(4);
+
+        // for line in input {
+        //     let ranges = line.splitn(2, ',');
+        //     let ranges = line
+        //         .splitn(2, ',')
+        //         .map(|range| range.splitn(2, '-').map(|id| id.parse::<i32>().ok()));
+        //     for mut range in ranges {
+        //         // let mut ids = range.splitn(2, '-').map(|id| id.parse::<i32>().ok());
+        //         // let range: RangeInclusive<i32> = ids.next()??..=ids.next()??;
+        //         // for id in ids {
+        //         //     let id_number = id.parse::<i32>().ok()?;
+        //         // }
+        //     }
+        // }
+        // input.map(|line| {
+        //     line.split(',')
+        //         .map(|range| range.split('-').map(|id| id.parse::<i32>()))
+        // });
+        // todo!()
     }
 
     fn part2(input: I) -> Option<i32> {
