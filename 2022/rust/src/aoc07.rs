6ccfbaf94a469809a22d31f216f2ea61fda96c86
xehpuk <xehpuk.dev@gmail.com>
2022-12-09 03:56:23 +0100

    rust: 2022.07: add terminal output parser

--- 2022/rust/src/aoc07.rs
+++ 2022/rust/src/aoc07.rs
@@ -1,15 +1,93 @@
 use super::Day;
 
+use std::collections::HashMap;
+use std::ops::ControlFlow;
+use ControlFlow::{Break, Continue};
+use Element::{Directory, File};
+
 pub struct Day07;
 
+#[derive(Debug)]
+enum Element {
+    File { size: u64 },
+    Directory { children: HashMap<String, Element> },
+}
+
+impl Element {
+    fn navigate(&mut self, path: &[String]) -> Option<&mut Self> {
+        match path.iter().try_fold(self, |destination, path| {
+            if let Directory { children } = destination {
+                if let Some(next) = children.get_mut(path) {
+                    return Continue(next);
+                }
+            }
+            Break(())
+        }) {
+            Continue(destination) => Some(destination),
+            Break(_) => None,
+        }
+    }
+}
+
 impl<'a, I: Clone + Iterator<Item = &'a str>> Day<'a, I, u64> for Day07 {
-    fn part1(_input: I) -> Option<u64> {
+    fn part1(input: I) -> Option<u64> {
+        println!("{:#?}", parse_terminal_output(input)?);
         todo!()
     }
 
     fn part2(_input: I) -> Option<u64> {
         todo!()
     }
+}
+
+fn parse_terminal_output<'a, I: Iterator<Item = &'a str>>(input: I) -> Option<Element> {
+    let mut root = Directory {
+        children: HashMap::new(),
+    };
+    let mut path = vec![];
+    for line in input {
+        let mut parts = line.split(' ');
+        let first = parts.next()?;
+        let second = parts.next()?;
+        match first {
+            "$" => match second {
+                "cd" => {
+                    let destination = parts.next()?;
+                    match destination {
+                        "/" => {
+                            path.clear();
+                        }
+                        ".." => {
+                            path.pop();
+                        }
+                        &_ => {
+                            path.push(destination.to_string());
+                        }
+                    }
+                }
+                "ls" => {}
+                &_ => return None,
+            },
+            _ => {
+                let mut cwd = root.navigate(&path)?;
+                if let Directory { children } = &mut cwd {
+                    children.insert(
+                        second.into(),
+                        match first {
+                            "dir" => Directory {
+                                children: HashMap::new(),
+                            },
+                            file_size => File {
+                                size: file_size.parse().ok()?,
+                            },
+                        },
+                    );
+                }
+            }
+        }
+    }
+
+    Some(root)
 }
 
 #[cfg(test)]
