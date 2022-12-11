2cd5580711163decb33d7c2bea395164582a340d
xehpuk <xehpuk.dev@gmail.com>
2022-12-11 03:13:20 +0100

    rust: 2022.09: add motion parsing

--- 2022/rust/src/aoc09.rs
+++ 2022/rust/src/aoc09.rs
@@ -1,9 +1,68 @@
 use super::Day;
+use std::collections::HashSet;
+use std::str::FromStr;
+
+use Direction::{Down, Left, Right, Up};
 
 pub struct Day09;
 
+struct Motion {
+    direction: Direction,
+    steps: i32,
+}
+
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+struct MotionParseError;
+
+impl FromStr for Direction {
+    type Err = MotionParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "L" => Ok(Left),
+            "R" => Ok(Right),
+            "U" => Ok(Up),
+            "D" => Ok(Down),
+            &_ => Err(MotionParseError),
+        }
+    }
+}
+
+impl FromStr for Motion {
+    type Err = MotionParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut iter = s.split(' ');
+        let direction = iter.next().ok_or(MotionParseError)?.parse()?;
+        let steps = iter
+            .next()
+            .ok_or(MotionParseError)?
+            .parse()
+            .or(Err(MotionParseError))?;
+
+        Ok(Motion { direction, steps })
+    }
+}
+
 impl<'a, I: Clone + Iterator<Item = &'a str>> Day<'a, I, usize> for Day09 {
-    fn part1(_input: I) -> Option<usize> {
+    fn part1(input: I) -> Option<usize> {
+        let mut head = (0, 0);
+        let mut tail = (0, 0);
+        let mut tail_positions: HashSet<(i32, i32)> = HashSet::new();
+        for motion in input.map(str::parse::<Motion>) {
+            match motion.ok()?.direction {
+                Left => {}
+                Right => {}
+                Up => {}
+                Down => {}
+            }
+        }
         todo!()
     }
 
