937f9c5599ad54ae74af24be79f41bc171e33b68
xehpuk <xehpuk.dev@gmail.com>
2022-12-14 05:32:25 +0100

    javascript: 2022.13, part 2: solve

--- 2022/javascript/aoc13.js
+++ 2022/javascript/aoc13.js
@@ -1,23 +1,62 @@
 import { open } from 'node:fs/promises'
 import { join } from 'node:path'
 
-const file = await open(join('..', '13.txt'))
-const lines = file.readLines()[Symbol.asyncIterator]()
+await part1()
+await part2()
 
-for (let i = 1, sum = 0;; i++) {
-    const first = await lines.next()
-    const second = await lines.next()
+async function part1() {
+    const file = await open(join('..', '13.txt'))
+    const lines = file.readLines()[Symbol.asyncIterator]()
 
-    lines.next() // skip empty
+    for (let i = 1, sum = 0;; i++) {
+        const first = await lines.next()
+        const second = await lines.next()
 
-    if (first.done || second.done) {
-        console.log(sum)
-        break
+        lines.next() // skip empty
+
+        if (first.done || second.done) {
+            console.log(sum)
+            break
+        }
+
+        if (compare(JSON.parse(first.value), JSON.parse(second.value)) <= 0) {
+            sum += i
+        }
+    }
+}
+
+async function part2() {
+    const firstDivider = JSON.parse('[[2]]');
+    const secondDivider = JSON.parse('[[6]]');
+
+    const file = await open(join('..', '13.txt'))
+    const lineIterator = file.readLines()[Symbol.asyncIterator]()
+    const packets = []
+
+    while (true) {
+        const first = await lineIterator.next()
+        const second = await lineIterator.next()
+
+        lineIterator.next() // skip empty
+
+        if (first.done || second.done) {
+            packets.push(firstDivider, secondDivider)
+            break
+        }
+
+        packets.push(JSON.parse(first.value), JSON.parse(second.value))
     }
 
-    if (compare(JSON.parse(first.value), JSON.parse(second.value)) <= 0) {
-        sum += i
+    packets.sort(compare)
+
+    function findIndex(divider) {
+        return packets.findIndex((packet) => compare(packet, divider) === 0) + 1
     }
+
+    const firstIndex = findIndex(firstDivider)
+    const secondIndex = findIndex(secondDivider)
+
+    console.log(firstIndex * secondIndex)
 }
 
 function compare(left, right) {
