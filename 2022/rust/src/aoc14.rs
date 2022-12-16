b6825c38720f7063b8668b29fb5a0e605ffbfd08
xehpuk <xehpuk.dev@gmail.com>
2022-12-16 03:31:21 +0100

    rust: 2022.14: parse paths

--- 2022/rust/src/aoc14.rs
+++ 2022/rust/src/aoc14.rs
@@ -1,9 +1,51 @@
+use std::num::ParseIntError;
+use std::str::Split;
+
 use super::Day;
 
 pub struct Day14;
 
+struct Path<'a, I>(I)
+where
+    I: Iterator<Item = &'a str>;
+
+impl<'a> Path<'a, Split<'a, &str>> {
+    fn new(s: &'a str) -> Self {
+        Path(s.split(" -> "))
+    }
+}
+
+impl<'a, I> Iterator for Path<'a, I>
+where
+    I: Iterator<Item = &'a str>,
+{
+    type Item = Result<(u32, u32), ParseIntError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut inner = self.0.next()?.split(',');
+        let mut parse = || inner.next().map(str::parse);
+
+        let x: u32 = match parse()? {
+            Ok(x) => x,
+            Err(e) => return Some(Err(e)),
+        };
+        let y: u32 = match parse()? {
+            Ok(x) => x,
+            Err(e) => return Some(Err(e)),
+        };
+
+        Some(Ok((x, y)))
+    }
+}
+
 impl<'a, I: Clone + Iterator<Item = &'a str>> Day<'a, I, u32> for Day14 {
-    fn part1(_input: I) -> Option<u32> {
+    fn part1(input: I) -> Option<u32> {
+        for path in input.map(Path::new) {
+            for point in path.flat_map(Result::ok) {
+                print!("{point:?}")
+            }
+            println!()
+        }
         todo!()
     }
 
@@ -14,8 +56,9 @@ impl<'a, I: Clone + Iterator<Item = &'a str>> Day<'a, I, u32> for Day14 {
 
 #[cfg(test)]
 mod tests {
-    use super::{Day, Day14};
     use std::str::Lines;
+
+    use super::{Day, Day14};
 
     const INPUT: &str = "\
 498,4 -> 498,6 -> 496,6
