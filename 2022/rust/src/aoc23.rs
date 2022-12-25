d9bdbb08a61cdb139e4b2804eb9e991d01ff5799
xehpuk <xehpuk.dev@gmail.com>
2022-12-25 01:14:08 +0100

    rust: 2022.23: divide

--- 2022/rust/src/aoc23.rs
+++ 2022/rust/src/aoc23.rs
@@ -1,20 +1,42 @@
+use std::collections::HashSet;
+
 use super::Day;
 
 pub struct Day23;
 
-type Number = i32;
+type Number = usize;
+type Elf = (i32, i32);
 
 impl<'a, I> Day<'a, I, Number> for Day23
 where
     I: Clone + Iterator<Item = &'a str>,
 {
-    fn part1(_input: I) -> Option<Number> {
-        todo!()
+    fn part1(input: I) -> Option<Number> {
+        let mut elves = parse_elves(input)?;
+        for round in 0..10 {
+            spread_out(&mut elves, round);
+        }
+        Some(count_empty_tiles(&elves))
     }
 
     fn part2(_input: I) -> Option<Number> {
         todo!()
     }
+}
+
+fn parse_elves<'a, I>(input: I) -> Option<HashSet<Elf>>
+where
+    I: Iterator<Item = &'a str>,
+{
+    todo!()
+}
+
+fn spread_out(elves: &mut HashSet<Elf>, round: i32) {
+    todo!()
+}
+
+fn count_empty_tiles(elves: &HashSet<Elf>) -> usize {
+    todo!()
 }
 
 #[cfg(test)]
