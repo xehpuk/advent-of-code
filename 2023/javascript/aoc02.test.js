9a158327d595579f3780152610c4ea1c4c754ab4
xehpuk <xehpuk.dev@gmail.com>
2023-12-02 13:47:05 +0100

    javascript: 2023.02: init

---
+++ 2023/javascript/aoc02.test.js
@@ -0,0 +1,10 @@
+import { part1, part2 } from './aoc02.js';
+
+test('day 02, part 1', async () => {
+    const received = await part1('02_test')
+    expect(received).toBe(8);
+});
+
+test('day 02, part 2', async () => {
+    expect('TODO').toBeFalsy()
+});
