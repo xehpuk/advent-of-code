d25eca0a7331526989bf212bab2da7788f25ed2a
xehpuk <xehpuk.dev@gmail.com>
2023-12-02 14:42:54 +0100

    javascript: 2023.02, part 1: solve

--- 2023/javascript/aoc02.js
+++ 2023/javascript/aoc02.js
@@ -1,7 +1,36 @@
 import { withLines } from './utils.js';
 
 export function part1(fileName = '02') {
-    throw new Error('TODO')
+    const bag = {
+        red: 12,
+        green: 13,
+        blue: 14,
+    }
+
+    function parseLine(line) {
+        const [game, semicolonSeparatedListOfSubsetsOfCubes] = line.split(': ')
+        const [, gameId] = game.split(' ')
+        const listOfSubsetsOfCubes = semicolonSeparatedListOfSubsetsOfCubes.split('; ')
+        const subsetsOfCubes = listOfSubsetsOfCubes.flatMap(subsetsOfCubes => subsetsOfCubes.split(', '))
+        const cubes = subsetsOfCubes.map(subsetOfCubes => subsetOfCubes.split(' '))
+            .reduce((cubes, [count, color]) => ({
+                ...cubes,
+                [color]: Math.max((cubes[color] ?? 0), Number.parseInt(count))
+            }), {})
+
+        return {
+            id: Number.parseInt(gameId),
+            cubes
+        }
+    }
+
+    return withLines(fileName, (value, line) => {
+        const game = parseLine(line)
+        if (Object.entries(game.cubes).every(([color, count]) => bag[color] >= count)) {
+            return value + game.id
+        }
+        return value
+    }, 0)
 }
 
 export function part2(fileName = '02') {
--- 2023/javascript/index.js
+++ 2023/javascript/index.js
@@ -1,4 +1,7 @@
 import * as day01 from './aoc01.js';
+import * as day02 from './aoc02.js';
 
 console.log('day 01, part 1:', await day01.part1())
 console.log('day 01, part 2:', await day01.part2())
+console.log('day 02, part 1:', await day02.part1())
+console.log('day 02, part 2:', await day02.part2())
