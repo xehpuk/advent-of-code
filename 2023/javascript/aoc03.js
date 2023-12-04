cf12e229de77736b359785b8c986e371ca48b5a3
xehpuk <xehpuk.dev@gmail.com>
2023-12-04 03:07:14 +0100

    javascript: 2023.03: formatting

--- 2023/javascript/aoc03.js
+++ 2023/javascript/aoc03.js
@@ -25,12 +25,13 @@ export async function part1(fileName = '03') {
         symbolPositions: [],
     })
 
-    return engineSchematic.partNumbers.flatMap((partNumbers, j) =>
-        partNumbers.filter(({n, i}) => engineSchematic.symbolPositions[j].includes(i - 1) ||
-            engineSchematic.symbolPositions[j].includes(i + n.length) ||
-            engineSchematic.symbolPositions[j - 1] && Array(n.length + 2).fill().some((_, k) => engineSchematic.symbolPositions[j - 1].includes(i + k - 1)) ||
-            engineSchematic.symbolPositions[j + 1] && Array(n.length + 2).fill().some((_, k) => engineSchematic.symbolPositions[j + 1].includes(i + k - 1)))
-    )
+    return engineSchematic.partNumbers
+        .flatMap((partNumbers, j) =>
+            partNumbers.filter(({n, i}) => engineSchematic.symbolPositions[j].includes(i - 1) ||
+                engineSchematic.symbolPositions[j].includes(i + n.length) ||
+                engineSchematic.symbolPositions[j - 1] && Array(n.length + 2).fill().some((_, k) => engineSchematic.symbolPositions[j - 1].includes(i + k - 1)) ||
+                engineSchematic.symbolPositions[j + 1] && Array(n.length + 2).fill().some((_, k) => engineSchematic.symbolPositions[j + 1].includes(i + k - 1)))
+        )
         .map(({n}) => Number.parseInt(n))
         .reduce((sum, partNumber) => sum + partNumber, 0)
 }
@@ -60,17 +61,18 @@ export async function part2(fileName = '03') {
         potentialGearPositions: [],
     })
 
-    return engineSchematic.potentialGearPositions.flatMap((potentialGearPositions, j) => {
-        return potentialGearPositions.map(potentialGearPosition => {
-            const adjacentPartNumbers = [
-                ...engineSchematic.potentialPartNumbers[j].filter(({n, i}) => i + n.length === potentialGearPosition || i === potentialGearPosition + 1),
-                ...(engineSchematic.potentialPartNumbers[j - 1] ?? []).filter(({n, i}) => i <= potentialGearPosition + 1 && potentialGearPosition - 1 < i + n.length),
-                ...(engineSchematic.potentialPartNumbers[j + 1] ?? []).filter(({n, i}) => i <= potentialGearPosition + 1 && potentialGearPosition - 1 < i + n.length),
-            ]
-            return adjacentPartNumbers.length === 2
-                ? adjacentPartNumbers[0].n * adjacentPartNumbers[1].n
-                : 0
-        })
-    })
+    return engineSchematic.potentialGearPositions
+        .flatMap((potentialGearPositions, j) =>
+            potentialGearPositions.map(potentialGearPosition => {
+                const adjacentPartNumbers = [
+                    ...engineSchematic.potentialPartNumbers[j].filter(({n, i}) => i + n.length === potentialGearPosition || i === potentialGearPosition + 1),
+                    ...(engineSchematic.potentialPartNumbers[j - 1] ?? []).filter(({n, i}) => i <= potentialGearPosition + 1 && potentialGearPosition - 1 < i + n.length),
+                    ...(engineSchematic.potentialPartNumbers[j + 1] ?? []).filter(({n, i}) => i <= potentialGearPosition + 1 && potentialGearPosition - 1 < i + n.length),
+                ]
+                return adjacentPartNumbers.length === 2
+                    ? adjacentPartNumbers[0].n * adjacentPartNumbers[1].n
+                    : 0
+            })
+        )
         .reduce((sum, gearRatio) => sum + gearRatio, 0)
 }
