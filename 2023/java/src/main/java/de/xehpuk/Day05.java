ca82d3f09dccd526439882e7aad38e4646217fc7
xehpuk <xehpuk.dev@gmail.com>
2023-12-06 01:06:10 +0100

    java: 2023.05, part 1: huge optimization

--- 2023/java/src/main/java/de/xehpuk/Day05.java
+++ 2023/java/src/main/java/de/xehpuk/Day05.java
@@ -8,24 +8,23 @@ import java.util.stream.Stream;
 public class Day05 {
     private static final Pattern NUMBER_REGEX = Pattern.compile("\\d+");
 
-    record Almanac(List<Long> seeds, List<NavigableMap<Long, SourceAndLength>> conversionMaps) {
+    record Almanac(List<Long> seeds, List<NavigableMap<Long, DestinationAndLength>> conversionMaps) {
     }
 
-    record SourceAndLength(long source, long length) {
+    record DestinationAndLength(long destination, long length) {
     }
 
     public static long part1(Stream<String> lines) {
-        Almanac almanac = parseAlmanac(lines);
-        var seeds = Set.copyOf(almanac.seeds());
-
-        return Utils.infiniteLongStream().filter(destinationId -> {
-            long seed = findSource(destinationId, almanac);
-            return seeds.contains(seed);
-        }).findFirst().getAsLong();
+        Almanac almanac = parseAlmanac(lines, true);
+        return almanac.seeds()
+                .parallelStream()
+                .mapToLong(seed -> findDestination(seed, almanac))
+                .min()
+                .getAsLong();
     }
 
     public static long part2(Stream<String> lines) {
-        Almanac almanac = parseAlmanac(lines);
+        Almanac almanac = parseAlmanac(lines, false);
 
         NavigableMap<Long, Long> seeds = new TreeMap<>();
         {
@@ -37,35 +36,39 @@ public class Day05 {
             }
         }
 
-        return Utils.infiniteLongStream().filter(destinationId -> {
-            long seed = findSource(destinationId, almanac);
+        return Utils.infiniteLongStream().filter(sourceId -> {
+            long seed = findDestination(sourceId, almanac);
             var seedRange = seeds.floorEntry(seed);
             return seedRange != null && seed < seedRange.getValue();
         }).findFirst().getAsLong();
     }
 
-    private static Long findSource(long destinationId, Almanac almanac) {
-        return almanac.conversionMaps().reversed().stream().reduce(destinationId, (value, conversionMap) -> {
+    private static long findDestination(long sourceId, Almanac almanac) {
+        return almanac.conversionMaps().stream().reduce(sourceId, (value, conversionMap) -> {
             var conversion = conversionMap.floorEntry(value);
             if (conversion == null) {
                 return value;
             }
-            long destinationStart = conversion.getKey();
-            long destinationEnd = destinationStart + conversion.getValue().length();
-            if (destinationEnd < value) {
+            long sourceStart = conversion.getKey();
+            long sourceEnd = sourceStart + conversion.getValue().length();
+            if (sourceEnd < value) {
                 return value;
             }
-            return value - destinationStart + conversion.getValue().source();
+            return value - sourceStart + conversion.getValue().destination();
         }, Utils.dummyCombiner());
     }
 
-    private static Almanac parseAlmanac(Stream<String> lines) {
+    private static Almanac parseAlmanac(Stream<String> lines, boolean forward) {
         return lines.reduce(new Almanac(List.of(), new ArrayList<>()), (value, line) -> {
             if (value.seeds.isEmpty()) {
                 return new Almanac(List.copyOf(extractNumbers(line)), value.conversionMaps());
             }
             if (line.isEmpty()) {
-                value.conversionMaps().add(new TreeMap<>());
+                if (forward) {
+                    value.conversionMaps().add(new TreeMap<>());
+                } else {
+                    value.conversionMaps().addFirst(new TreeMap<>());
+                }
                 return value;
             }
             if (line.endsWith(":")) {
@@ -75,7 +78,11 @@ public class Day05 {
             long destinationId = numbers.get(0);
             long sourceId = numbers.get(1);
             long rangeLength = numbers.get(2);
-            value.conversionMaps().getLast().put(destinationId, new SourceAndLength(sourceId, rangeLength));
+            if (forward) {
+                value.conversionMaps().getLast().put(sourceId, new DestinationAndLength(destinationId, rangeLength));
+            } else {
+                value.conversionMaps().getFirst().put(destinationId, new DestinationAndLength(sourceId, rangeLength));
+            }
             return value;
         }, Utils.dummyCombiner());
     }
