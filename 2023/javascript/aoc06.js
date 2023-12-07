482d1afbd2013bdd5cac2c1fe294f13a43d315c8
xehpuk <xehpuk.dev@gmail.com>
2023-12-07 21:11:19 +0100

    javascript: 2023.06, part 2: simplify

--- 2023/javascript/aoc06.js
+++ 2023/javascript/aoc06.js
@@ -5,12 +5,12 @@ function createHandleLine(parseLine) {
 }
 
 /**
- * {@link https://www.wolframalpha.com/input?i=solve+d%3D%28t-x%29*x+for+x|WolframAlpha}
+ * {@link https://www.wolframalpha.com/input?i=solve+d%3C%28t-x%29*x+for+x|WolframAlpha}
  */
 function calcNumberOfWins(time, distance) {
-    const z = Math.sqrt(time ** 2 - 4 * distance)
-    const time0 = Math.floor((time - z) / 2 + 1)
-    const time1 = Math.ceil((time + z) / 2 - 1)
+    const z = Math.sqrt(time ** 2 - 4 * (distance + 1))
+    const time0 = Math.ceil((time - z) / 2)
+    const time1 = Math.floor((time + z) / 2)
     return time1 - time0 + 1
 }
 
