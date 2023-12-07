b6efb4a56357ca8fca6d868b4e0e13cedb208b97
xehpuk <xehpuk.dev@gmail.com>
2023-12-07 20:43:36 +0100

    javascript: 2023.06, part 2: solve

--- 2023/javascript/aoc06.js
+++ 2023/javascript/aoc06.js
@@ -1,21 +1,23 @@
 import {withLines} from './utils.js'
 
-function parseLine(line) {
-    return line.match(/\d+/g).map(n => +n)
+function createHandleLine(parseLine) {
+    return (numbers, line) => [...numbers, parseLine(line)]
+}
+
+function calcNumberOfWins(time, distance) {
+    const z = Math.sqrt(time ** 2 - 4 * distance)
+    const time0 = Math.floor((time - z) / 2 + 1)
+    const time1 = Math.ceil((time + z) / 2 - 1)
+    return time1 - time0 + 1
 }
 
 export function part1(fileName = '06') {
-    return withLines(fileName, (numbers, line) => [...numbers, parseLine(line)], [])
+    return withLines(fileName, createHandleLine(line => line.match(/\d+/g).map(n => +n)), [])
         .then(([times, distances]) =>
-            times.reduce((product, time, i) => {
-                const distance = distances[i]
-                const z = Math.sqrt(time ** 2 - 4 * distance)
-                const time0 = Math.floor((time - z) / 2 + 1)
-                const time1 = Math.ceil((time + z) / 2 - 1)
-                return product * (time1 - time0 + 1)
-            }, 1))
+            times.reduce((product, time, i) => product * calcNumberOfWins(time, distances[i]), 1))
 }
 
 export function part2(fileName = '06') {
-    throw new Error('TODO')
+    return withLines(fileName, createHandleLine(line => +line.replace(/\D/g, '')), [])
+        .then(([time, distance]) => calcNumberOfWins(time, distance))
 }
