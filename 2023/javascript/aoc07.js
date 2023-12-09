a986e84bdb7ae692c5e1ca92f1e4b454e1afd083
xehpuk <xehpuk.dev@gmail.com>
2023-12-09 19:37:56 +0100

    javascript: 2023.07: refactoring

--- 2023/javascript/aoc07.js
+++ 2023/javascript/aoc07.js
@@ -1,5 +1,9 @@
 import {withLines} from './utils.js'
 
+const JOKER = 'J'
+
+const cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+
 const handTypes = [
     /(.)\1{4}/, // Five of a kind
     /(.)\1{3}/, // Four of a kind
@@ -9,6 +13,10 @@ const handTypes = [
     /(.)\1/, // One pair
     /./, // High card
 ].map((type, i, types) => [type.test.bind(type), types.length - i])
+
+function calcCardStrengths(cards) {
+    return new Map(cards.map((card, i, cards) => [card, cards.length - i]))
+}
 
 function calcHand(hand) {
     const normalizedHand = Array.from(hand).toSorted().join('')
@@ -42,27 +50,25 @@ function createHandleLine(calcHand, cardStrengths) {
 }
 
 export function part1(fileName = '07') {
-    const cardStrengths = new Map(['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-        .map((card, i, cards) => [card, cards.length - i]))
-
-    return withLines(fileName, createHandleLine(calcHand, cardStrengths), []).then(calcTotalWinnings)
+    return withLines(fileName, createHandleLine(calcHand, calcCardStrengths(cards)), []).then(calcTotalWinnings)
 }
 
 export function part2(fileName = '07') {
-    const cardStrengths = new Map(['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
-        .map((card, i, cards) => [card, cards.length - i]))
+    const cardStrengths = calcCardStrengths(cards
+        .filter(card => card !== JOKER)
+        .concat(JOKER))
 
     function calcHand2(hand) {
-        if (!hand.includes('J')) {
+        if (!hand.includes(JOKER)) {
             return calcHand(hand)
         }
         const [cardReplacement] = Object.entries(Array.from(hand)
-            .filter(card => card !== 'J')
+            .filter(card => card !== JOKER)
             .reduce((cards, card) => ({
                 ...cards,
                 [card]: (cards[card] ?? 0) + 1,
-            }), {})).reduce((prev, curr) => prev[1] >= curr[1] ? prev : curr, ['J', 0])
-        return calcHand(hand.replaceAll('J', cardReplacement))
+            }), {})).reduce((prev, curr) => prev[1] >= curr[1] ? prev : curr, [JOKER, 0])
+        return calcHand(hand.replaceAll(JOKER, cardReplacement))
     }
 
     return withLines(fileName, createHandleLine(calcHand2, cardStrengths), []).then(calcTotalWinnings)
