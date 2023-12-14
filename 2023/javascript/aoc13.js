e5b6d34f3351538c5bc3bb1297b3d8079335eba1
xehpuk <xehpuk.dev@gmail.com>
2023-12-14 15:25:52 +0100

    javascript: 2023.13: refactoring

--- 2023/javascript/aoc13.js
+++ 2023/javascript/aoc13.js
@@ -1,76 +1,48 @@
 import {withLines} from './utils.js'
 
-function calcNote1(pattern) {
-    const maxRow = pattern.length - 1
-    r: for (let row = 0; row < maxRow; row++) {
-        const length = Math.min(maxRow - row, row + 1)
-        for (let y = 0; y < length; y++) {
-            const originalRow = pattern[row - y]
-            const mirroredRow = pattern[row + y + 1]
-            if (originalRow !== mirroredRow) {
-                continue r
-            }
-        }
-        return 100 * (row + 1)
-    }
-
-    const maxColumn = pattern[0].length - 1
-    c: for (let column = 0; column < maxColumn; column++) {
-        const length = Math.min(maxColumn - column, column + 1)
-        for (let x = 0; x < length; x++) {
-            for (const row of pattern) {
-                const originalColumn = row[column - x]
-                const mirroredColumn = row[column + x + 1]
-                if (originalColumn !== mirroredColumn) {
-                    continue c
-                }
-            }
-        }
-        return column + 1
-    }
-}
-
-function calcNote2(pattern) {
-    const maxRow = pattern.length - 1
-    r: for (let row = 0; row < maxRow; row++) {
-        const length = Math.min(maxRow - row, row + 1)
-        let fixedSmudge = false
-        for (let y = 0; y < length; y++) {
-            const maxX = pattern[0].length
-            for (let x = 0; x < maxX; x++) {
-                const originalRow = pattern[row - y][x]
-                const mirroredRow = pattern[row + y + 1][x]
-                if (originalRow !== mirroredRow) {
-                    if (fixedSmudge) {
-                        continue r
+function calcNote(expectedSmudges) {
+    return pattern => {
+        const maxRow = pattern.length - 1
+        r: for (let row = 0; row < maxRow; row++) {
+            const length = Math.min(maxRow - row, row + 1)
+            let fixedSmudges = 0
+            for (let y = 0; y < length; y++) {
+                const maxX = pattern[0].length
+                for (let x = 0; x < maxX; x++) {
+                    const originalRow = pattern[row - y][x]
+                    const mirroredRow = pattern[row + y + 1][x]
+                    if (originalRow !== mirroredRow) {
+                        if (fixedSmudges >= expectedSmudges) {
+                            continue r
+                        }
+                        fixedSmudges++
                     }
-                    fixedSmudge = true
                 }
             }
+            if (fixedSmudges === expectedSmudges) {
+                return 100 * (row + 1)
+            }
         }
-        if (fixedSmudge) {
-            return 100 * (row + 1)
-        }
-    }
 
-    const maxColumn = pattern[0].length - 1
-    c: for (let column = 0; column < maxColumn; column++) {
-        const length = Math.min(maxColumn - column, column + 1)
-        let fixedSmudge = false
-        for (let x = 0; x < length; x++) {
-            for (const row of pattern) {
-                const originalColumn = row[column - x]
-                const mirroredColumn = row[column + x + 1]
-                if (originalColumn !== mirroredColumn) {
-                    if (fixedSmudge) {
-                        continue c
+        const maxColumn = pattern[0].length - 1
+        c: for (let column = 0; column < maxColumn; column++) {
+            const length = Math.min(maxColumn - column, column + 1)
+            let fixedSmudges = 0
+            for (let x = 0; x < length; x++) {
+                for (const row of pattern) {
+                    const originalColumn = row[column - x]
+                    const mirroredColumn = row[column + x + 1]
+                    if (originalColumn !== mirroredColumn) {
+                        if (fixedSmudges >= expectedSmudges) {
+                            continue c
+                        }
+                        fixedSmudges++
                     }
-                    fixedSmudge = true
                 }
             }
-        }
-        if (fixedSmudge) {
-            return column + 1
+            if (fixedSmudges === expectedSmudges) {
+                return column + 1
+            }
         }
     }
 }
@@ -94,9 +66,9 @@ function summarize(fileName) {
 }
 
 export function part1(fileName = '13') {
-    return summarize(fileName)(calcNote1)
+    return summarize(fileName)(calcNote(0))
 }
 
 export function part2(fileName = '13') {
-    return summarize(fileName)(calcNote2)
+    return summarize(fileName)(calcNote(1))
 }
