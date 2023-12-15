f00f61b06d8974eb2cd72e68c7f98d990c3024a2
xehpuk <xehpuk.dev@gmail.com>
2023-12-15 19:35:00 +0100

    javascript: 2023.14, part 1: refactoring

--- 2023/javascript/aoc14.js
+++ 2023/javascript/aoc14.js
@@ -1,38 +1,58 @@
 import {withLines} from './utils.js'
 
-const regex = /[O#]/g
+const ROUND = 1
+const CUBE = -1
+const EMPTY = 0
 
-function parseRow(line) {
-    const rocks = {
-        'O': new Set(),
-        '#': new Set(),
+function parseRow(row) {
+    return Array.from(row).map(stone => {
+        switch (stone) {
+            case 'O': return ROUND
+            case '#': return CUBE
+            default: return EMPTY
+        }
+    })
+}
+
+function tiltNorth(platform) {
+    for (let i = 1; i < platform.length; i++) {
+        for (let j = i; j > 0; j--) {
+            const currentRow = platform[j]
+            const northernRow = platform[j - 1]
+            let moved = false
+            for (let k = 0; k < currentRow.length; k++) {
+                if (currentRow[k] === ROUND && northernRow[k] === EMPTY) {
+                    northernRow[k] = currentRow[k]
+                    currentRow[k] = EMPTY
+                    moved = true
+                }
+            }
+            if (!moved) {
+                break
+            }
+        }
     }
-    for (const rock of line.matchAll(regex)) {
-        rocks[rock[0]].add(rock.index)
+}
+
+function calcTotalLoad(platform) {
+    let totalLoad = 0
+    for (let i = 0; i < platform.length; i++) {
+        const load = platform.length - i
+        for (const rock of platform[i]) {
+            if (rock === ROUND) {
+                totalLoad += load
+            }
+        }
     }
-    return rocks
+    return totalLoad
 }
 
 export function part1(fileName = '14') {
     return withLines(fileName, (platform, row) => [...platform, parseRow(row)], [])
         .then(platform => {
-            for (let i = 1; i < platform.length; i++) {
-                for (let j = i; j > 0; j--) {
-                    const currentPlatform = platform[j]
-                    const upperPlatform = platform[j - 1]
-                    for (const roundedRock of currentPlatform['O']) {
-                        if (!upperPlatform['O'].has(roundedRock) && !upperPlatform['#'].has(roundedRock)) {
-                            currentPlatform['O'].delete(roundedRock)
-                            upperPlatform['O'].add(roundedRock)
-                        }
-                    }
-                }
-            }
-            let sum = 0
-            for (let i = 0; i < platform.length; i++) {
-                sum += platform[i]['O'].size * (platform.length - i)
-            }
-            return sum
+            tiltNorth(platform)
+
+            return calcTotalLoad(platform)
         })
 }
 
