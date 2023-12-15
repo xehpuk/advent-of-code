653ef68cbcf9c9f18925e2689619f6e0deca1d9a
xehpuk <xehpuk.dev@gmail.com>
2023-12-15 18:48:48 +0100

    javascript: 2023.15, part 2: solve

--- 2023/javascript/aoc15.js
+++ 2023/javascript/aoc15.js
@@ -1,5 +1,9 @@
 import {withLines} from './utils.js'
 
+function parseLine(line) {
+    return line.split(',')
+}
+
 function hash(step) {
     let hash = 0
     for (let i = 0; i < step.length; i++) {
@@ -9,11 +13,52 @@ function hash(step) {
 }
 
 export function part1(fileName = '15') {
-    return withLines(fileName, (sum, line) => line.split(',')
-        .map(hash)
-        .reduce((sum, hash) => sum + hash, sum), 0)
+    return withLines(fileName, (sum, line) => parseLine(line)
+            .map(hash)
+            .reduce((sum, hash) => sum + hash, sum),
+        0)
 }
 
 export function part2(fileName = '15') {
-    return withLines(fileName, (lines, line, index) => [...lines, line], [])
+    function parseStep(step) {
+        const [label] = step.match(/[a-z]+/)
+        const operation = step[label.length]
+        const focalLength = +step.substring(label.length + 1)
+
+        return {
+            label,
+            operation,
+            focalLength,
+        }
+    }
+
+    function executeStep(boxes, step) {
+        const box = boxes[hash(step.label)] ??= []
+        const labelIndex = box.findIndex(lens => lens.label === step.label)
+        switch (step.operation) {
+            case '=':
+                if (labelIndex !== -1) {
+                    box[labelIndex].focalLength = step.focalLength
+                } else {
+                    box.push(step)
+                }
+                break
+            case '-':
+                if (labelIndex !== -1) {
+                    box.splice(labelIndex, 1)
+                }
+                break
+        }
+        return boxes
+    }
+
+    function calcFocusingPower(sum, box, i) {
+        return box.reduce((sum, lens, slot) => sum + (1 + i) * (1 + slot) * lens.focalLength, sum)
+    }
+
+    return withLines(fileName, (sum, line) => parseLine(line)
+            .map(parseStep)
+            .reduce(executeStep, [])
+            .reduce(calcFocusingPower, 0),
+        0)
 }
