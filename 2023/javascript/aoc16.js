d669c1d35899a8c3a23bc4dad2d0eb833f1363da
xehpuk <xehpuk.dev@gmail.com>
2023-12-16 11:17:51 +0100

    javascript: 2023.16: improve performance

--- 2023/javascript/aoc16.js
+++ 2023/javascript/aoc16.js
@@ -1,14 +1,16 @@
 import {withLines} from './utils.js'
 
-function equals(tile1, tile2) {
-    return tile1.x === tile2.x &&
-        tile1.y === tile2.y &&
-        tile1.dx === tile2.dx &&
-        tile1.dy === tile2.dy
+function stringifyTile(tile) {
+    return `${tile.x},${tile.y}`
+}
+
+function stringifyBeam(beam) {
+    return `${beam.x},${beam.y},${beam.dx},${beam.dy}`
 }
 
 function energize(contraption, initialBeam) {
-    const energizedTiles = []
+    const energizedTiles = new Set()
+    const beamTrail = new Set()
     const beams = [initialBeam]
 
     while (beams.length > 0) {
@@ -16,58 +18,61 @@ function energize(contraption, initialBeam) {
         beam.x += beam.dx
         beam.y += beam.dy
         if (beam.y >= 0 && beam.y < contraption.length &&
-            beam.x >= 0 && beam.x < contraption[beam.y].length &&
-            !energizedTiles.some(tile => equals(tile, beam))) {
-            energizedTiles.push({...beam})
+            beam.x >= 0 && beam.x < contraption[beam.y].length) {
+            const beamId = stringifyBeam(beam)
+            if (!beamTrail.has(beamId)) {
+                beamTrail.add(beamId)
+                energizedTiles.add(stringifyTile(beam))
 
-            switch (contraption[beam.y][beam.x]) {
-                case '|':
-                    if (beam.dx !== 0) {
-                        beams.push({
-                            ...beam,
-                            dx: 0,
-                            dy: 1,
-                        })
-                        beam.dx = 0
-                        beam.dy = -1
-                    }
-                    break
-                case '-':
-                    if (beam.dy !== 0) {
-                        beams.push({
-                            ...beam,
-                            dx: 1,
-                            dy: 0,
-                        })
-                        beam.dx = -1
-                        beam.dy = 0
-                    }
-                    break
-                case '\\':
-                    if (beam.dx !== 0) {
-                        beam.dy = beam.dx
-                        beam.dx = 0
-                    } else {
-                        beam.dx = beam.dy
-                        beam.dy = 0
-                    }
-                    break
-                case '/':
-                    if (beam.dx !== 0) {
-                        beam.dy = -beam.dx
-                        beam.dx = 0
-                    } else {
-                        beam.dx = -beam.dy
-                        beam.dy = 0
-                    }
-                    break
+                switch (contraption[beam.y][beam.x]) {
+                    case '|':
+                        if (beam.dx !== 0) {
+                            beams.push({
+                                ...beam,
+                                dx: 0,
+                                dy: 1,
+                            })
+                            beam.dx = 0
+                            beam.dy = -1
+                        }
+                        break
+                    case '-':
+                        if (beam.dy !== 0) {
+                            beams.push({
+                                ...beam,
+                                dx: 1,
+                                dy: 0,
+                            })
+                            beam.dx = -1
+                            beam.dy = 0
+                        }
+                        break
+                    case '\\':
+                        if (beam.dx !== 0) {
+                            beam.dy = beam.dx
+                            beam.dx = 0
+                        } else {
+                            beam.dx = beam.dy
+                            beam.dy = 0
+                        }
+                        break
+                    case '/':
+                        if (beam.dx !== 0) {
+                            beam.dy = -beam.dx
+                            beam.dx = 0
+                        } else {
+                            beam.dx = -beam.dy
+                            beam.dy = 0
+                        }
+                        break
+                }
+                continue
             }
-        } else {
-            beams.splice(0, 1)
         }
+        beams.splice(0, 1)
     }
 
-    return energizedTiles.reduce((uniqueTiles, tile) => uniqueTiles.add(`${tile.x},${tile.y}`), new Set()).size
+    return energizedTiles.size
 }
 
 export function part1(fileName = '16') {
