7f652387500bca6d7e094062270e16e3cd56336f
xehpuk <xehpuk.dev@gmail.com>
2023-12-20 05:12:47 +0100

    javascript: 2023.19, part 2: solve

--- 2023/javascript/aoc19.js
+++ 2023/javascript/aoc19.js
@@ -86,5 +86,95 @@ export function part1(fileName = '19') {
 }
 
 export function part2(fileName = '19') {
-    return withLines(fileName, (lines, line, index) => [...lines, line], [])
+    function parseWorkflow(workflows, line) {
+        function parseRule(rule) {
+            const [check, destination] = rule.split(':')
+
+            if (destination === undefined) {
+                return {
+                    transform: {
+                        pos: part => part,
+                        neg: _ => null,
+                    },
+                    destination: check,
+                }
+            }
+
+            const property = check[0]
+            const operator = check[1]
+            const value = +check.substring(2)
+            const template = transform => part => ({
+                    ...part,
+                    [property]: {
+                        ...part[property],
+                        ...transform(part),
+                    },
+                }
+            )
+            const transform = operator === '<'
+                ? {
+                    pos: template(part => ({
+                        max: Math.min(part[property].max, value - 1),
+                    })),
+                    neg: template(part => ({
+                        min: Math.max(part[property].min, value),
+                    })),
+                }
+                : {
+                    pos: template(part => ({
+                        min: Math.max(part[property].min, value + 1),
+                    })),
+                    neg: template(part => ({
+                        max: Math.min(part[property].max, value),
+                    })),
+                }
+
+            return {
+                transform,
+                destination,
+            }
+        }
+
+        const nameLength = line.indexOf('{')
+        const name = line.substring(0, nameLength)
+        const rules = line.substring(nameLength + 1, line.length - 1).split(',').map(parseRule)
+
+        return {
+            ...workflows,
+            [name]: rules,
+        }
+    }
+
+    let parse = parseWorkflow
+    return withLines(fileName, (workflows, line) => {
+        if (line.length === 0) {
+            parse = workflow => workflow
+            return workflows
+        }
+        return parse(workflows, line)
+    }, {}).then(workflows => {
+        const queue = [{
+            workflowName: 'in',
+            part: Array.from('xmas').reduce((part, rating) => ({
+                ...part,
+                [rating]: {
+                    min: 1,
+                    max: 4000,
+                },
+            }), {}),
+        }]
+        for (let i = 0; i < queue.length; i++) {
+            let {workflowName, part} = queue[i]
+            for (const rule of workflows[workflowName] ?? []) {
+                queue.push({
+                    workflowName: rule.destination,
+                    part: rule.transform.pos(part),
+                })
+                part = rule.transform.neg(part)
+            }
+        }
+        return queue.filter(item => item.workflowName === 'A')
+            .reduce((sum, item) => sum + Object.values(item.part)
+                .reduce((product, {min, max}) => product * (max - min + 1), 1), 0)
+    })
 }
