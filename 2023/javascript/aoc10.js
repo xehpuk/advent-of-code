e5f890e47011cb8b54b4e946aeb0c2983d9f220a
xehpuk <xehpuk.dev@gmail.com>
2023-12-22 03:05:42 +0100

    javascript: 2023.10, part 2: improve performance

--- 2023/javascript/aoc10.js
+++ 2023/javascript/aoc10.js
@@ -11,6 +11,12 @@ const tileDirections = {
     'F': ['E', 'S'],
 }
 
+const tilesPointingNorth = new Set(
+    Object.entries(tileDirections)
+        .filter(([, directions]) => directions.includes('N'))
+        .map(([tile]) => tile),
+)
+
 const directions = {
     E: ([lat, long]) => [lat, long + 1],
     S: ([lat, long]) => [lat + 1, long],
@@ -78,11 +84,15 @@ export function part2(fileName = '10') {
             ],
         }
     }, {tiles: []}).then(({start, tiles}) => {
+        const loop = []
         let tile = (() => {
             for (const [direction, move] of Object.entries(directions)) {
                 const [nextY, nextX] = move(start)
                 const label = tiles[nextY][nextX]
                 if (tileDirections[label].includes(opposites[direction])) {
+                    loop[nextY] = []
+                    loop[nextY][nextX] = tilesPointingNorth.has(label)
+
                     return {
                         direction,
                         nextY,
@@ -92,35 +102,33 @@ export function part2(fileName = '10') {
                 }
             }
         })()
-        const loop = [tile]
         while (tile.label !== startLabel) {
             const direction = tileDirections[tile.label].find(direction => direction !== opposites[tile.direction])
             const [nextY, nextX] = directions[direction]([tile.nextY, tile.nextX])
             const label = tiles[nextY][nextX]
+
+            loop[nextY] ??= []
+            loop[nextY][nextX] = tilesPointingNorth.has(label)
+
             tile = {
                 direction,
                 nextY,
                 nextX,
                 label,
             }
-            loop.push(tile)
         }
-
-        const tilesPointingNorth = new Set(
-            Object.entries(tileDirections)
-                .filter(([, directions]) => directions.includes('N'))
-                .map(([tile]) => tile),
-        )
 
         let interior = 0
         for (let y = 0; y < tiles.length; y++) {
+            const row = loop[y]
+            if (row === undefined) {
+                continue
+            }
             let numberOfPipes = 0
-            for (let x = 0; x < tiles[y].length; x++) {
-                const pipe = loop.find(({nextY, nextX}) => nextY === y && nextX === x)
-                if (pipe) {
-                    if (tilesPointingNorth.has(pipe.label)) {
-                        numberOfPipes ^= 1
-                    }
+            for (let x = 0; x < tiles[y].length - 1; x++) {
+                const pipe = row[x]
+                if (pipe !== undefined) {
+                    numberOfPipes ^= pipe
                 } else {
                     interior += numberOfPipes
                 }
