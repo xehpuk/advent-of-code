ac3cabc9e2d8b802743ec7db52236365b777d924
xehpuk <xehpuk.dev@gmail.com>
2024-12-06 08:09:33 +0100

    java: 2024.06, part 2: improve performance only check obstructions on original path

--- 2024/java/src/main/java/de/xehpuk/adventofcode/Day06.java
+++ 2024/java/src/main/java/de/xehpuk/adventofcode/Day06.java
@@ -3,25 +3,39 @@ package de.xehpuk.adventofcode;
 import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class Day06 {
     public static long part1(final Stream<String> lines) {
-        final var map = lines
-                .toList();
-        var guard = findGuard(map);
+        final var map = lines.toList();
+        final var guard = findGuard(map);
 
-        final var visitedPositions = new HashSet<>();
+        return visitedPositionsBeforeLeave(map, guard).size();
+    }
+
+    public static long part2(final Stream<String> lines) {
+        final var map = lines.toList();
+        final var guard = findGuard(map);
+
+        return visitedPositionsBeforeLeave(map, guard).stream()
+                .filter(obstruction -> loops(map, guard, obstruction))
+                .count();
+    }
+
+    private static Set<Position> visitedPositionsBeforeLeave(final List<String> map, final Guard initialGuard) {
+        var guard = initialGuard;
+        final var visitedPositions = new HashSet<Position>();
         visitedPositions.add(guard.position);
         while (true) {
             var old = guard;
             guard = guard.move();
             if (guard.position.y < 0 || guard.position.y >= map.size()) {
-                return visitedPositions.size();
+                return visitedPositions;
             }
             final var row = map.get(guard.position.y);
             if (guard.position.x < 0 || guard.position.x >= row.length()) {
-                return visitedPositions.size();
+                return visitedPositions;
             }
             if (row.charAt(guard.position.x) == '#') {
                 guard = old.turnRight();
@@ -29,23 +43,6 @@ public class Day06 {
             }
             visitedPositions.add(guard.position);
         }
-    }
-
-    public static long part2(final Stream<String> lines) {
-        final var map = lines
-                .toList();
-        final var guard = findGuard(map);
-
-        var sum = 0;
-        for (int y = 0; y < map.size(); y++) {
-            final var row = map.get(y);
-            for (int x = 0; x < row.length(); x++) {
-                if (row.charAt(x) != '#' && loops(map, guard, new Position(x, y))) {
-                    sum++;
-                }
-            }
-        }
-        return sum;
     }
 
     private static Guard findGuard(final List<String> map) {
@@ -103,23 +100,30 @@ public class Day06 {
             this.dx = dx;
             this.dy = dy;
         }
+
+        Direction rotateClockwise() {
+            return switch (this) {
+                case UP -> RIGHT;
+                case RIGHT -> DOWN;
+                case DOWN -> LEFT;
+                case LEFT -> UP;
+            };
+        }
     }
 
     record Position(int x, int y) {
+        Position move(final Direction direction) {
+            return new Position(x + direction.dx, y + direction.dy);
+        }
     }
 
     record Guard(Position position, Direction direction) {
         Guard move() {
-            return new Guard(new Position(position.x + direction.dx, position.y + direction.dy), direction);
+            return new Guard(position.move(direction), direction);
         }
 
         Guard turnRight() {
-            return new Guard(new Position(position.x, position.y), switch (direction) {
-                case UP -> Direction.RIGHT;
-                case RIGHT -> Direction.DOWN;
-                case DOWN -> Direction.LEFT;
-                case LEFT -> Direction.UP;
-            });
+            return new Guard(position, direction.rotateClockwise());
         }
     }
 }
