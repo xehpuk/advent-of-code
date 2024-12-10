873f94519f04b22bea59e5924eb2d4d141e7e00f
xehpuk <xehpuk.dev@gmail.com>
2024-12-10 20:26:17 +0100

    java: 2024.10: solve

--- 2024/java/src/main/java/de/xehpuk/adventofcode/Day10.java
+++ 2024/java/src/main/java/de/xehpuk/adventofcode/Day10.java
@@ -1,13 +1,58 @@
 package de.xehpuk.adventofcode;
 
+import de.xehpuk.adventofcode.Utils.II;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day10 {
     public static long part1(final Stream<String> lines) {
-        return 0L;
+        return solve(lines, Collectors.toSet());
     }
 
     public static long part2(final Stream<String> lines) {
-        return 0L;
+        return solve(lines, Collectors.toList());
+    }
+
+    private static <C extends Collection<II>> long solve(final Stream<String> lines, final Collector<II, ?, C> collector) {
+        final var grid = Utils.parseGrid(lines);
+        final Map<Character, Set<II>> map = Utils.mapByR(grid.elements(), HashSet::new);
+
+        long sum = 0;
+        for (final var trailhead : map.get('0')) {
+            Collection<II> reachablePositions = List.of(trailhead);
+            for (int i = 1; i < 10; i++) {
+                final char height = (char) ('0' + i);
+                final var nextPositions = map.get(height);
+                reachablePositions = reachablePositions.stream()
+                        .flatMap(reachablePosition -> Arrays.stream(Direction.values())
+                                .<II>mapMulti((direction, consumer) -> {
+                                    final var nextPosition = new II(reachablePosition.l() + direction.dx, reachablePosition.r() + direction.dy);
+                                    if (nextPositions.contains(nextPosition)) {
+                                        consumer.accept(nextPosition);
+                                    }
+                                }))
+                        .collect(collector);
+            }
+            sum += reachablePositions.size();
+        }
+        return sum;
+    }
+
+    enum Direction {
+        UP(0, -1),
+        RIGHT(1, 0),
+        DOWN(0, 1),
+        LEFT(-1, 0);
+
+        private final int dx;
+        private final int dy;
+
+        Direction(final int dx, final int dy) {
+            this.dx = dx;
+            this.dy = dy;
+        }
     }
 }
--- 2024/java/src/test/java/de/xehpuk/adventofcode/Day10Test.java
+++ 2024/java/src/test/java/de/xehpuk/adventofcode/Day10Test.java
@@ -7,51 +7,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day10Test {
     @Test
     void part1_1() {
-        assertEquals(1L, Utils.withLines("10_1_test", Day10::part1));
+        assertEquals(1L, Utils.withLines("10_1_1_test", Day10::part1));
     }
 
     @Test
     void part1_2() {
-        assertEquals(2L, Utils.withLines("10_2_test", Day10::part1));
+        assertEquals(2L, Utils.withLines("10_1_2_test", Day10::part1));
     }
 
     @Test
     void part1_3() {
-        assertEquals(4L, Utils.withLines("10_3_test", Day10::part1));
+        assertEquals(4L, Utils.withLines("10_1_3_test", Day10::part1));
     }
 
     @Test
     void part1_4() {
-        assertEquals(3L, Utils.withLines("10_4_test", Day10::part1));
+        assertEquals(3L, Utils.withLines("10_1_4_test", Day10::part1));
     }
 
     @Test
     void part1_5() {
-        assertEquals(36L, Utils.withLines("10_5_test", Day10::part1));
+        assertEquals(36L, Utils.withLines("10_test", Day10::part1));
     }
 
     @Test
     void part2_1() {
-        assertEquals(0L, Utils.withLines("10_1_test", Day10::part2));
+        assertEquals(3L, Utils.withLines("10_2_1_test", Day10::part2));
     }
 
     @Test
     void part2_2() {
-        assertEquals(0L, Utils.withLines("10_2_test", Day10::part2));
+        assertEquals(13L, Utils.withLines("10_2_2_test", Day10::part2));
     }
 
     @Test
     void part2_3() {
-        assertEquals(0L, Utils.withLines("10_3_test", Day10::part2));
-    }
-
-    @Test
-    void part2_4() {
-        assertEquals(0L, Utils.withLines("10_4_test", Day10::part2));
-    }
-
-    @Test
-    void part2_5() {
-        assertEquals(0L, Utils.withLines("10_5_test", Day10::part2));
+        assertEquals(81L, Utils.withLines("10_test", Day10::part2));
     }
 }
