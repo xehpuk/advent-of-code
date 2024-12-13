834dd13cea5d3c50fc2d6818e4e33a83a8347d77
xehpuk <xehpuk.dev@gmail.com>
2024-12-13 02:04:52 +0100

    java: 2024.12, part 2: init

--- 2024/java/src/test/java/de/xehpuk/adventofcode/Day12Test.java
+++ 2024/java/src/test/java/de/xehpuk/adventofcode/Day12Test.java
@@ -1,6 +1,5 @@
 package de.xehpuk.adventofcode;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,9 +20,28 @@ public class Day12Test {
         assertEquals(1930L, Utils.withLines("12_3_test", Day12::part1));
     }
 
-    @Disabled
     @Test
-    void part2() {
-        assertEquals(0L, Utils.withLines("12_test", Day12::part2));
+    void part2_1() {
+        assertEquals(80L, Utils.withLines("12_1_test", Day12::part2));
+    }
+
+    @Test
+    void part2_2() {
+        assertEquals(436L, Utils.withLines("12_2_test", Day12::part2));
+    }
+
+    @Test
+    void part2_3() {
+        assertEquals(1206L, Utils.withLines("12_3_test", Day12::part2));
+    }
+
+    @Test
+    void part2_4() {
+        assertEquals(236L, Utils.withLines("12_4_test", Day12::part2));
+    }
+
+    @Test
+    void part2_5() {
+        assertEquals(368L, Utils.withLines("12_5_test", Day12::part2));
     }
 }
