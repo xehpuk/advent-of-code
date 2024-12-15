e366827132a0cee54229416b5b469fbf727c065a
xehpuk <xehpuk.dev@gmail.com>
2024-12-15 03:09:04 +0100

    java: 2024.14, part 2: "solve"

--- 2024/java/src/main/java/de/xehpuk/adventofcode/Day14.java
+++ 2024/java/src/main/java/de/xehpuk/adventofcode/Day14.java
@@ -3,16 +3,21 @@ package de.xehpuk.adventofcode;
 import de.xehpuk.adventofcode.Utils.II;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Gatherers;
 import java.util.stream.Stream;
 
 public class Day14 {
+    private static final int WIDTH = 101;
+    private static final int HEIGHT = 103;
+
     private static final Pattern INTS = Pattern.compile("-?\\d+");
 
     public static long part1(final Stream<String> lines) {
-        return part1(lines, 101, 103);
+        return part1(lines, WIDTH, HEIGHT);
     }
 
     static long part1(final Stream<String> lines, final int width, final int height) {
@@ -47,7 +52,38 @@ public class Day14 {
     }
 
     public static long part2(final Stream<String> lines) {
-        return 0L;
+        var robots = lines.map(Day14::parseLine).toList();
+        for (int i = 0; ; i++) {
+            if (looksLikeXmasTree(robots)) {
+                return i;
+            }
+            robots = robots.stream()
+                    .map(robot -> new Robot(robot.p().add(robot.v()).mod(WIDTH, HEIGHT), robot.v()))
+                    .toList();
+        }
+    }
+
+    /**
+     * Randomly checks whether the robots don't overlap. How did part 2 pass QA?
+     */
+    private static boolean looksLikeXmasTree(final List<Robot> robots) {
+        final int width = WIDTH;
+        final int height = HEIGHT;
+        final var ps = robots.stream()
+                .map(Robot::p)
+                .collect(Collectors.toSet());
+        final boolean xmasTree = robots.size() == ps.size();
+        if (xmasTree) {
+            final var builder = new StringBuilder((width + 1) * height);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    builder.append(ps.contains(new II(x, y)) ? 'R' : '.');
+                }
+                builder.append('\n');
+            }
+            System.out.println(builder);
+        }
+        return xmasTree;
     }
 
     private static Robot parseLine(final String line) {
--- 2024/java/src/test/java/de/xehpuk/adventofcode/Day14Test.java
+++ 2024/java/src/test/java/de/xehpuk/adventofcode/Day14Test.java
@@ -1,6 +1,5 @@
 package de.xehpuk.adventofcode;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.stream.Stream;
@@ -11,11 +10,5 @@ public class Day14Test {
     @Test
     void part1() {
         assertEquals(12L, Utils.withLines("14_test", (Stream<String> value) -> Day14.part1(value, 11, 7)));
-    }
-
-    @Tag("template")
-    @Test
-    void part2() {
-        assertEquals(0L, Utils.withLines("14_test", Day14::part2));
     }
 }
