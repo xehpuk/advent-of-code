e33c499e8e6294aa5d4ca9950a11063fd06f428e
xehpuk <xehpuk.dev@gmail.com>
2024-12-15 14:10:03 +0100

    java: 2024.15, part 2: init

--- 2024/java/src/test/java/de/xehpuk/adventofcode/Day15Test.java
+++ 2024/java/src/test/java/de/xehpuk/adventofcode/Day15Test.java
@@ -1,6 +1,5 @@
 package de.xehpuk.adventofcode;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,9 +15,8 @@ public class Day15Test {
         assertEquals(2028L, Utils.withLines("15_2_test", Day15::part1));
     }
 
-    @Tag("template")
     @Test
     void part2() {
-        assertEquals(0L, Utils.withLines("15_test", Day15::part2));
+        assertEquals(9021L, Utils.withLines("15_1_test", Day15::part2));
     }
 }
