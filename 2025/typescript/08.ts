007c75f8d1d1d13db829bb3a89bdb2e54db3dc3e
xehpuk <xehpuk.dev@gmail.com>
2025-12-08 19:19:32 +0100

    deno: 2025.08: refactoring (accepting a performance hit in part 2)

--- 2025/typescript/08.ts
+++ 2025/typescript/08.ts
@@ -1,58 +1,20 @@
 import { insertSorted, product, toLines } from "./util.ts";
 
 export function part1(input: string, n: number = 1000): string {
-  const junctionBoxes: Point3d[] = toLines(input).map((line) => {
-    const [x, y, z] = line.match(/\d+/g)!.map(Number);
-    return { x, y, z };
-  });
-  const minDistances: {
-    junctionBox: Point3d;
-    junctionBox2: Point3d;
-    distance: number;
-  }[] = [];
-  for (let i = 0; i < junctionBoxes.length - 1; i++) {
-    const junctionBox = junctionBoxes[i];
-    for (let j = i + 1; j < junctionBoxes.length; j++) {
-      const junctionBox2 = junctionBoxes[j];
-      const distance = euclidianDistance(junctionBox, junctionBox2);
+  const junctionBoxes = parseJunctionBoxes(input);
+  const minConnections = calcConnections(
+    junctionBoxes,
+    (connections: Connection[], connection: Connection) =>
       insertSorted(
-        minDistances,
-        {
-          junctionBox,
-          junctionBox2,
-          distance,
-        },
-        (junctionBox, junctionBox2) =>
-          junctionBox.distance - junctionBox2.distance,
+        connections,
+        connection,
+        (connection, connection2) => connection.distance - connection2.distance,
         n,
-      );
-    }
-  }
-  const circuits: Set<Point3d>[] = junctionBoxes.map((junctionBox) =>
-    new Set([junctionBox])
+      ),
   );
-  for (const { junctionBox, junctionBox2 } of minDistances) {
-    const circuitIndex = circuits.findIndex((circuit) =>
-      circuit.has(junctionBox)
-    );
-    const circuitIndex2 = circuits.findIndex((circuit) =>
-      circuit.has(junctionBox2)
-    );
-    if (circuitIndex === circuitIndex2) {
-      continue;
-    }
-    let minIndex: number;
-    let maxIndex: number;
-    if (circuitIndex < circuitIndex2) {
-      minIndex = circuitIndex;
-      maxIndex = circuitIndex2;
-    } else {
-      minIndex = circuitIndex2;
-      maxIndex = circuitIndex;
-    }
-    circuits[minIndex] = circuits[minIndex].union(
-      circuits.splice(maxIndex, 1)[0],
-    );
+  const circuits = initCircuits(junctionBoxes);
+  for (const { junctionBox, junctionBox2 } of minConnections) {
+    connect(circuits, junctionBox, junctionBox2);
   }
   circuits.sort((circuit1, circuit2) => circuit2.size - circuit1.size);
   return product(circuits.map((circuit) => circuit.size).slice(0, 3))
@@ -60,74 +22,94 @@ export function part1(input: string, n: number = 1000): string {
 }
 
 export function part2(input: string): string {
-  console.time("parsing");
-  const junctionBoxes: Point3d[] = toLines(input).map((line) => {
-    const [x, y, z] = line.match(/\d+/g)!.map(Number);
-    return { x, y, z };
-  });
-  console.timeEnd("parsing");
-  const distances: {
-    junctionBox: Point3d;
-    junctionBox2: Point3d;
-    distance: number;
-  }[] = Array(junctionBoxes.length * (junctionBoxes.length - 1) / 2);
-  console.time("insertion");
-  let c = 0;
-  for (let i = 0; i < junctionBoxes.length - 1; i++) {
-    const junctionBox = junctionBoxes[i];
-    for (let j = i + 1; j < junctionBoxes.length; j++) {
-      const junctionBox2 = junctionBoxes[j];
-      const distance = euclidianDistance(junctionBox, junctionBox2);
-      distances[c++] = {
-        junctionBox,
-        junctionBox2,
-        distance,
-      };
-    }
-  }
-  console.timeEnd("insertion");
-  console.time("sorting")
-  distances.sort((junctionBox, junctionBox2) =>
-    junctionBox.distance - junctionBox2.distance);
-  console.timeEnd("sorting")
-  const circuits: Set<Point3d>[] = junctionBoxes.map((junctionBox) =>
-    new Set([junctionBox])
+  const junctionBoxes = parseJunctionBoxes(input);
+  const connections = calcConnections(
+    junctionBoxes,
+    (connections: Connection[], connection: Connection) =>
+      connections.push(connection),
   );
-  console.time("connecting");
-  for (const { junctionBox, junctionBox2 } of distances) {
-    const circuitIndex = circuits.findIndex((circuit) =>
-      circuit.has(junctionBox)
-    );
-    const circuitIndex2 = circuits.findIndex((circuit) =>
-      circuit.has(junctionBox2)
-    );
-    if (circuitIndex === circuitIndex2) {
-      continue;
-    }
-    let minIndex: number;
-    let maxIndex: number;
-    if (circuitIndex < circuitIndex2) {
-      minIndex = circuitIndex;
-      maxIndex = circuitIndex2;
-    } else {
-      minIndex = circuitIndex2;
-      maxIndex = circuitIndex;
-    }
-    circuits[minIndex] = circuits[minIndex].union(
-      circuits.splice(maxIndex, 1)[0],
-    );
+  connections.sort((junctionBox, junctionBox2) =>
+    junctionBox.distance - junctionBox2.distance
+  );
+  const circuits = initCircuits(junctionBoxes);
+  for (const { junctionBox, junctionBox2 } of connections) {
+    connect(circuits, junctionBox, junctionBox2);
     if (circuits.length === 1) {
-      console.timeEnd("connecting");
       return (junctionBox.x * junctionBox2.x).toString();
     }
   }
   throw new Error("unreachable");
 }
 
+function parseJunctionBoxes(input: string): Point3d[] {
+  return toLines(input).map((line) => {
+    const [x, y, z] = line.match(/\d+/g)!.map(Number);
+    return { x, y, z };
+  });
+}
+
+function initCircuits(junctionBoxes: Point3d[]): Set<Point3d>[] {
+  return junctionBoxes.map((junctionBox) => new Set([junctionBox]));
+}
+
+function calcConnections(
+  junctionBoxes: Point3d[],
+  insertionFn: (connections: Connection[], connection: Connection) => void,
+): Connection[] {
+  const connections: Connection[] = [];
+  for (let i = 0; i < junctionBoxes.length - 1; i++) {
+    const junctionBox = junctionBoxes[i];
+    for (let j = i + 1; j < junctionBoxes.length; j++) {
+      const junctionBox2 = junctionBoxes[j];
+      const distance = euclidianDistance(junctionBox, junctionBox2);
+      insertionFn(connections, {
+        junctionBox,
+        junctionBox2,
+        distance,
+      });
+    }
+  }
+  return connections;
+}
+
+function connect(
+  circuits: Set<Point3d>[],
+  junctionBox: Point3d,
+  junctionBox2: Point3d,
+) {
+  const circuitIndex = circuits.findIndex((circuit) =>
+    circuit.has(junctionBox)
+  );
+  const circuitIndex2 = circuits.findIndex((circuit) =>
+    circuit.has(junctionBox2)
+  );
+  if (circuitIndex === circuitIndex2) {
+    return;
+  }
+  let minIndex: number;
+  let maxIndex: number;
+  if (circuitIndex < circuitIndex2) {
+    minIndex = circuitIndex;
+    maxIndex = circuitIndex2;
+  } else {
+    minIndex = circuitIndex2;
+    maxIndex = circuitIndex;
+  }
+  circuits[minIndex] = circuits[minIndex].union(
+    circuits.splice(maxIndex, 1)[0],
+  );
+}
+
 function euclidianDistance(p1: Point3d, p2: Point3d): number {
   return Math.sqrt(
     (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2,
   );
+}
+
+interface Connection {
+  junctionBox: Point3d;
+  junctionBox2: Point3d;
+  distance: number;
 }
 
 interface Point3d {
