3ff6ae22e4b0a1d997517e8d93c7b3c59195bf34
xehpuk <xehpuk.dev@gmail.com>
2025-12-10 00:38:21 +0100

    deno: 2025.09, part 2: solve slowly (several seconds)

--- 2025/typescript/09.ts
+++ 2025/typescript/09.ts
@@ -7,19 +7,13 @@ export function part1(input: string): string {
     .toString();
 }
 
-// FIXME: too slow
 export function part2(input: string): string {
   const tiles = parseTiles(input);
-  const dimension = calcDimension(tiles);
-  console.log(dimension);
   const lines = calcLines(tiles);
-  console.log(lines);
   const rectangles = calcRectangles(tiles);
   rectangles.sort((r1, r2) => r2.area - r1.area);
   for (const rectangle of rectangles) {
-    console.log(rectangle);
-    if (containsOnlyRedOrGreen(rectangle, lines, dimension)) {
-      console.log(rectangle);
+    if (containsOnlyRedOrGreen(rectangle, lines)) {
       return rectangle.area.toString();
     }
   }
@@ -77,80 +71,71 @@ function calcLines(points: Point[]): Lines {
   return lines;
 }
 
-function calcDimension(points: Point[]): Rectangle {
-  let minX = Infinity;
-  let maxX = -Infinity;
-  let minY = Infinity;
-  let maxY = -Infinity;
-  for (const point of points) {
-    if (point.x < minX) {
-      minX = point.x;
-    } else if (point.x > maxX) {
-      maxX = point.x;
-    }
-    if (point.y < minY) {
-      minY = point.y;
-    } else if (point.y > maxY) {
-      maxY = point.y;
-    }
-  }
-  const point = { x: minX, y: minY };
-  const point2 = { x: maxX, y: maxY };
-  return {
-    point: { x: minX, y: minY },
-    point2: { x: maxX, y: maxY },
-    area: calcArea(point, point2),
-  };
-}
-
-function isRedOrGreen(
-  point: Point,
-  lines: Lines,
-  dimension: Rectangle,
-): boolean {
-  if (isInRange(point.y, lines.x.get(point.x))) {
-    return true;
-  }
-  if (isInRange(point.x, lines.y.get(point.y))) {
-    return true;
-  }
-  let inside = false;
-  const y = point.y + 0.5;
-  for (let x = point.x + 1; x <= dimension.point2.x; x++) {
-    inside = inside !== isInRange(y, lines.x.get(x));
-  }
-  return inside;
-}
-
-function isInRange(n: number, range: Range | undefined): boolean {
-  if (range === undefined) {
-    return false;
-  }
-  return range.start <= n && n <= range.end;
-}
-
 function containsOnlyRedOrGreen(
   rectangle: Rectangle,
   lines: Lines,
-  dimension: Rectangle,
 ): boolean {
-  for (let x = rectangle.point.x; x <= rectangle.point2.x; x++) {
-    if (!isRedOrGreen({ x, y: rectangle.point.y }, lines, dimension)) {
+  for (let x = rectangle.point.x + 1; x < rectangle.point2.x; x++) {
+    if (isVerticalStart({ x, y: rectangle.point.y }, lines)) {
       return false;
     }
-    if (!isRedOrGreen({ x, y: rectangle.point2.y }, lines, dimension)) {
+    if (isVerticalEnd({ x, y: rectangle.point2.y }, lines)) {
       return false;
     }
   }
   for (let y = rectangle.point.y + 1; y < rectangle.point2.y; y++) {
-    if (!isRedOrGreen({ x: rectangle.point.x, y }, lines, dimension)) {
+    if (isHorizontalStart({ x: rectangle.point.x, y }, lines)) {
       return false;
     }
-    if (!isRedOrGreen({ x: rectangle.point2.x, y }, lines, dimension)) {
+    if (isHorizontalEnd({ x: rectangle.point2.x, y }, lines)) {
       return false;
     }
   }
   return true;
+}
+
+function isVerticalStart(
+  point: Point,
+  lines: Lines,
+): boolean {
+  const range = lines.x.get(point.x);
+  if (!range) {
+    return false;
+  }
+  return point.y >= range.start && point.y < range.end;
+}
+
+function isVerticalEnd(
+  point: Point,
+  lines: Lines,
+): boolean {
+  const range = lines.x.get(point.x);
+  if (!range) {
+    return false;
+  }
+  return point.y > range.start && point.y <= range?.end;
+}
+
+function isHorizontalStart(
+  point: Point,
+  lines: Lines,
+): boolean {
+  const range = lines.y.get(point.y);
+  if (!range) {
+    return false;
+  }
+  return point.x >= range.start && point.x < range.end;
+}
+
+function isHorizontalEnd(
+  point: Point,
+  lines: Lines,
+): boolean {
+  const range = lines.y.get(point.y);
+  if (!range) {
+    return false;
+  }
+  return point.x > range.start && point.x <= range?.end;
 }
 
 interface Lines {
--- 2025/typescript/main.ts
+++ 2025/typescript/main.ts
@@ -8,8 +8,12 @@ if (import.meta.main) {
   }
   const input = await readInput(day);
   const solver = await import(`./${day}.ts`);
+  console.time(`${day}.1`);
   const part1 = solver.part1(input);
+  console.timeEnd(`${day}.1`);
   console.log(`${day}.1:`, part1);
+  console.time(`${day}.2`);
   const part2 = solver.part2(input);
+  console.timeEnd(`${day}.2`);
   console.log(`${day}.2:`, part2);
 }
