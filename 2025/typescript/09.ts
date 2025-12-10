58979653d9a3bc5134a6daae0895763a2cfb4cf7
xehpuk <xehpuk.dev@gmail.com>
2025-12-10 01:52:18 +0100

    deno: 2025.09, part 2: improve performance (~100ms)

--- 2025/typescript/09.ts
+++ 2025/typescript/09.ts
@@ -1,23 +1,23 @@
 import { toLines } from "./util.ts";
 
 export function part1(input: string): string {
-  const tiles = parseTiles(input);
-  const rectangles = calcRectangles(tiles);
+  const points = parseTiles(input);
+  const rectangles = calcRectangles(points);
   return rectangles.reduce((max, rectangle) => Math.max(max, rectangle.area), 0)
     .toString();
 }
 
+/**
+ * @see {@link https://github.com/jackysee/aoc/blob/4101ab4/2025/day9.js|jackysee/aoc}
+ */
 export function part2(input: string): string {
-  const tiles = parseTiles(input);
-  const lines = calcLines(tiles);
-  const rectangles = calcRectangles(tiles);
-  rectangles.sort((r1, r2) => r2.area - r1.area);
-  for (const rectangle of rectangles) {
-    if (containsOnlyRedOrGreen(rectangle, lines)) {
-      return rectangle.area.toString();
-    }
-  }
-  throw new Error("unreachable");
+  const points: Point[] = parseTiles(input);
+  const rectangles: Rectangle[] = calcRectangles(points).sort((a, b) =>
+    b.area - a.area
+  );
+  const lines: Line[] = calcLines(points);
+  return rectangles.find((rectangle) => !intersectLines(lines, rectangle))!
+    .area.toString();
 }
 
 function parseTiles(input: string): Point[] {
@@ -27,17 +27,16 @@ function parseTiles(input: string): Point[] {
   });
 }
 
-function calcRectangles(tiles: Point[]): Rectangle[] {
+function calcRectangles(points: Point[]): Rectangle[] {
   const rectangles: Rectangle[] = [];
-  for (let i = 0; i < tiles.length - 1; i++) {
-    const tile = tiles[i];
-    for (let j = i + 1; j < tiles.length; j++) {
-      const tile2 = tiles[j];
-      const area = calcArea(tile, tile2);
+  for (let i = 0; i < points.length - 1; i++) {
+    const point = points[i];
+    for (let j = i + 1; j < points.length; j++) {
+      const point2 = points[j];
       rectangles.push({
-        point: { x: Math.min(tile.x, tile2.x), y: Math.min(tile.y, tile2.y) },
-        point2: { x: Math.max(tile.x, tile2.x), y: Math.max(tile.y, tile2.y) },
-        area,
+        point,
+        point2,
+        area: calcArea(point, point2),
       });
     }
   }
@@ -48,104 +47,34 @@ function calcArea(p1: Point, p2: Point): number {
   return (Math.abs(p1.x - p2.x) + 1) * (Math.abs(p1.y - p2.y) + 1);
 }
 
-function calcLines(points: Point[]): Lines {
-  const lines = {
-    x: new Map<number, Range>(),
-    y: new Map<number, Range>(),
-  };
-  let previous = points.at(-1)!;
-  for (const point of points) {
-    if (previous.x === point.x) {
-      const range = previous.y < point.y
-        ? { start: previous.y, end: point.y }
-        : { start: point.y, end: previous.y };
-      lines.x.set(point.x, range);
-    } else {
-      const range = previous.x < point.x
-        ? { start: previous.x, end: point.x }
-        : { start: point.x, end: previous.x };
-      lines.y.set(point.y, range);
-    }
-    previous = point;
+function calcLines(points: Point[]): Line[] {
+  const lines: Line[] = [];
+  let point = points.at(-1)!;
+  for (const point2 of points) {
+    lines.push({
+      point,
+      point2,
+    });
+    point = point2;
   }
   return lines;
 }
 
-function containsOnlyRedOrGreen(
-  rectangle: Rectangle,
-  lines: Lines,
-): boolean {
-  for (let x = rectangle.point.x + 1; x < rectangle.point2.x; x++) {
-    if (isVerticalStart({ x, y: rectangle.point.y }, lines)) {
-      return false;
-    }
-    if (isVerticalEnd({ x, y: rectangle.point2.y }, lines)) {
-      return false;
-    }
-  }
-  for (let y = rectangle.point.y + 1; y < rectangle.point2.y; y++) {
-    if (isHorizontalStart({ x: rectangle.point.x, y }, lines)) {
-      return false;
-    }
-    if (isHorizontalEnd({ x: rectangle.point2.x, y }, lines)) {
-      return false;
-    }
-  }
-  return true;
+function intersectLines(lines: Line[], rect: Rectangle): boolean {
+  return lines.some((line) =>
+    inRange(line.point.y, line.point2.y, rect.point.y, rect.point2.y) &&
+    inRange(line.point.x, line.point2.x, rect.point.x, rect.point2.x)
+  );
 }
 
-function isVerticalStart(
-  point: Point,
-  lines: Lines,
-): boolean {
-  const range = lines.x.get(point.x);
-  if (!range) {
-    return false;
-  }
-  return point.y >= range.start && point.y < range.end;
+function inRange(a1: number, a2: number, b1: number, b2: number): boolean {
+  return !(a1 <= b1 && a1 <= b2 && a2 <= b1 && a2 <= b2) &&
+    !(a1 >= b1 && a1 >= b2 && a2 >= b1 && a2 >= b2);
 }
 
-function isVerticalEnd(
-  point: Point,
-  lines: Lines,
-): boolean {
-  const range = lines.x.get(point.x);
-  if (!range) {
-    return false;
-  }
-  return point.y > range.start && point.y <= range?.end;
-}
-
-function isHorizontalStart(
-  point: Point,
-  lines: Lines,
-): boolean {
-  const range = lines.y.get(point.y);
-  if (!range) {
-    return false;
-  }
-  return point.x >= range.start && point.x < range.end;
-}
-
-function isHorizontalEnd(
-  point: Point,
-  lines: Lines,
-): boolean {
-  const range = lines.y.get(point.y);
-  if (!range) {
-    return false;
-  }
-  return point.x > range.start && point.x <= range?.end;
-}
-
-interface Lines {
-  x: Map<number, Range>;
-  y: Map<number, Range>;
-}
-
-interface Range {
-  start: number;
-  end: number;
+interface Line {
+  point: Point;
+  point2: Point;
 }
 
 interface Rectangle {
